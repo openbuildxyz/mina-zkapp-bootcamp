f3ff0d6ad135d253018a9d57533bfc0c8281cd7f
Leon <zhongchang.ding@gmail.com>
2024-12-04 16:59:55 +0800

    task2

--- learn/LeonTing1010/task3/crowdfunding/node_modules/o1js/src/lib/provable/types/util.ts
+++
@@ -1,8 +0,0 @@
-import { ProvableType } from './provable-intf.js';
-import { witness } from './witness.js';
-
-export { emptyWitness };
-
-function emptyWitness<T>(type: ProvableType<T>) {
-  return witness(type, () => ProvableType.synthesize(type));
-}
