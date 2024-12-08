446d9950b1327a4f50785d367135c20e84702a91
swbwawa <1193161065@qq.com>
2024-12-08 10:38:04 +0800

    Update crowdfunding-zkapp.ts

--- learn/swbwawa/task3/crowdfunding-zkapp.ts
+++ learn/swbwawa/task3/crowdfunding-zkapp.ts
@@ -54,5 +54,26 @@ import {
   
       return newX;
     }
+
+    //Withdraw
+    //withdraw all_funding_money to account
+    async withdraw() 
+    {
+      // 当前余额
+      const currentBalance = this.account.balance.getAndRequireEquals();
+      // const beneficiary = this.beneficiary.getAndRequireEquals();
+
+      // Only beneficiary
+      // this.sender.getAndRequireSignature().equals(beneficiary).assertTrue('Only beneficiary can withdraw');
+
+      // 余额不为0
+      currentBalance.greaterThan(UInt64.from(0)).assertTrue('Balance must be greater than 0');
+
+      // 发送至账号
+      // this.send({ to: beneficiary, amount: currentBalance });
+
+      // 提现事件
+      // this.emitEvent('amount', new AmountEvent({ type: Field(2), amount: currentBalance }));
+    }
   }
   
