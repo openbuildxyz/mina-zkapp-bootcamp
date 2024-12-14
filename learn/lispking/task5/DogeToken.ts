a4ec5f763d4b38e79f602862fdd88d7a5718a5f9
lispking <lispking@qq.com>
2024-12-14 10:02:18 +0800

    remove used code

--- learn/lispking/task5/DogeToken.ts
+++ learn/lispking/task5/DogeToken.ts
@@ -1,4 +1,4 @@
-import { TokenContract, UInt64, method, AccountUpdateForest, Permissions } from "o1js";
+import { TokenContract, UInt64, method, AccountUpdateForest } from "o1js";
 
 const SUPPLY = UInt64.from(10n ** 18n);
 export class DogeToken extends TokenContract {
@@ -9,15 +9,6 @@ export class DogeToken extends TokenContract {
   async deploy() {
     await super.deploy();
     this.account.tokenSymbol.set('DOGE');
-
-    // make account non-upgradable forever
-    this.account.permissions.set({
-      ...Permissions.default(),
-      setVerificationKey:
-        Permissions.VerificationKey.impossibleDuringCurrentVersion(),
-      setPermissions: Permissions.impossible(),
-      access: Permissions.proofOrSignature(),
-    });
   }
 
   @method async init() {
