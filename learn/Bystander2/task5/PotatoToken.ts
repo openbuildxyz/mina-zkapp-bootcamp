53ebcce3a7ea549f11a93fd9592908c63d5ad324
Ming <757352776@qq.com>
2024-12-23 19:57:47 +0800

    update task5

---
+++ learn/Bystander2/task5/PotatoToken.ts
@@ -0,0 +1,22 @@
+import { AccountUpdateForest, method, TokenContract, UInt64 } from 'o1js';
+
+const SUPPLY = UInt64.from(10n ** 18n);
+export class PotatoToken extends TokenContract {
+  @method
+  async approveBase(updates: AccountUpdateForest) {
+    this.checkZeroBalanceChange(updates);
+  }
+
+  async deploy() {
+    await super.deploy();
+    this.account.tokenSymbol.set('POTATO');
+  }
+
+  @method
+  async init() { 
+    super.init();
+
+    // mint the entire supply to the token account with the same address as this contract
+    this.internal.mint({ address: this.address, amount: SUPPLY });
+  }
+}
