cbc68d5af553a0f968bd6bcbda070213f39351da
Abner <128780147+cdt3211@users.noreply.github.com>
2024-12-23 17:49:31 +0800

    feat: finish task6 (#481)
    * feat: finish task4
    
    * feat: finish task6

--- learn/cdt3211/task4/crowFunding.test.ts
+++ learn/cdt3211/task4/crowFunding.test.ts
@@ -5,6 +5,7 @@ import {
   AccountUpdate,
   Mina,
   PrivateKey,
+  PublicKey,
   UInt32,
   UInt64
 } from 'o1js';
@@ -15,9 +16,10 @@ describe('CrowdfundingContract', () => {
   let creator: Mina.TestPublicKey;
   let zkApp: CrowdfundingContract;
   let zkAppAccount: PrivateKey;
+  let Local: any;
 
   beforeEach(async () => {
-    const Local = await Mina.LocalBlockchain({ proofsEnabled: false });
+    Local = await Mina.LocalBlockchain({ proofsEnabled: false });
     Mina.setActiveInstance(Local);
 
     [deployer, investor, creator] = Local.testAccounts;
@@ -39,6 +41,16 @@ describe('CrowdfundingContract', () => {
     await txn.sign([deployer.key, zkAppAccount]).send();
   }
 
+  // 账户资金释放(用向其他账户转账的形式代替)
+  async function release(amount: UInt64) {
+    const txn = await Mina.transaction(creator, async () => {
+      const accountUpdate = AccountUpdate.createSigned(creator);
+      accountUpdate.send({ to: investor, amount: amount });
+    });
+    await txn.prove();
+    await txn.sign([creator.key]).send();
+  }
+
   // 投资
   async function fund(amount: UInt64) {
     const txn = await Mina.transaction(investor, async () => {
@@ -46,8 +58,8 @@ describe('CrowdfundingContract', () => {
     });
     await txn.prove();
     await txn.sign([investor.key]).send();
-    return txn;
   }
+
 
   it('投资测试', async () => {
     await localDeploy();
@@ -64,6 +76,9 @@ describe('CrowdfundingContract', () => {
     const amount = UInt64.from(100);
     await fund(amount);
 
+    Local.setBlockchainLength(UInt32.from(10));
+    Mina.setActiveInstance(Local);
+
     // 非项目发起人无法提款
     expect(async () => {
       const txn = await Mina.transaction(investor, async () => {
@@ -73,14 +88,15 @@ describe('CrowdfundingContract', () => {
       await txn.sign([investor.key]).send();
     }).rejects;
 
+    const beforeBalance = Mina.getBalance(creator);
     //提款
-    const beforeBalance = AccountUpdate.create(creator).account.balance.get();
     const txn = await Mina.transaction(creator, async () => {
       await zkApp.withdraw();
     });
     await txn.prove();
     await txn.sign([creator.key]).send();
-    expect(AccountUpdate.create(creator).account.balance.get()).toEqual(beforeBalance.add(amount));
+
+    expect(Mina.getBalance(creator)).toEqual(beforeBalance.add(amount));
   })
 
   it('资金逐步释放', async () => {
@@ -88,23 +104,44 @@ describe('CrowdfundingContract', () => {
     const amount = UInt64.from(100);
     await fund(amount);
 
+    // 清空余额
+    const balance = Mina.getBalance(creator);
+    await release(balance);
+    expect(Mina.getBalance(creator)).toEqual(UInt64.from(0));
+
+    Local.setBlockchainLength(UInt32.from(100));
+    Mina.setActiveInstance(Local);
+
     //提款
-    const beforeBalance = AccountUpdate.create(creator).account.balance.get();
     const txn = await Mina.transaction(creator, async () => {
       await zkApp.withdraw();
     });
     await txn.prove();
     await txn.sign([creator.key]).send();
-    expect(AccountUpdate.create(creator).account.balance.get()).toEqual(beforeBalance.add(amount));
+    expect(Mina.getBalance(creator)).toEqual(UInt64.from(100));
 
-    //再次提款
+    //首次只能提取20%
+    await release(UInt64.from(Number(amount) * 0.2));
+
+    expect(Mina.getBalance(creator)).toEqual(UInt64.from(80));
+
+    // 再次提取会失败
     expect(async () => {
-      const txn = await Mina.transaction(creator, async () => {
-        await zkApp.withdraw();
-      });
-      await txn.prove();
-      await txn.sign([creator.key]).send();
+      await release(UInt64.from(1));
     }).rejects;
+
+    // 200个区块后
+    Local.incrementGlobalSlot(200);
+    // 释放10%
+    await release(UInt64.from(Number(amount) * 0.1));
+    expect(Mina.getBalance(creator)).toEqual(UInt64.from(70));
+
+    Local.incrementGlobalSlot(1400)
+    // 释放剩余全部
+    await release(UInt64.from(Number(amount) * 0.7));
+    expect(Mina.getBalance(creator)).toEqual(UInt64.from(0));
+
+
   })
 
 });
